af6cb13ff6e21aee39a4627d124d86cd3f32154d
anup30 <anupbarua30@gmail.com>
2023-12-05 16:53:55 +0600

    Initial commit

---
+++ bin/project_03.dart
@@ -0,0 +1,5 @@
+import 'package:project_03/project_03.dart' as project_03;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_03.calculate()}!');
+}
---
+++ test/project_03_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_03/project_03.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
